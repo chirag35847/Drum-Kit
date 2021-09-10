b74ba9d92e8dfcf06df78d7e059cca862ec150db
Chirag Mittal <chirag.workid@gmail.com>
2021-09-10 18:48:54 +0530

    button press animation

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ for(var i=0;i<document.querySelectorAll(".drum").length;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
         var buttonInnerHtml=this.innerHTML;
         makeSound(buttonInnerHtml);
+        buttonAnimation(buttonInnerHtml);
     });
 }
 
 document.addEventListener("keypress",function(event){
-    makeSound( event.key);
+    makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 function makeSound(key){
@@ -46,4 +48,12 @@ function makeSound(key){
             console.log(buttonInnerHtml);
             break;
     }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
 }
